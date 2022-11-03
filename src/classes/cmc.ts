aff015ac3d6117960c536e925a1d8c03c98abb44
Marcus Kamuntu <63915967+aspekts@users.noreply.github.com>
2022-11-03 13:09:21 +0000

    Documentation for CMC update, prod- ready

--- src/classes/cmc.ts
+++ src/classes/cmc.ts
@@ -1,15 +1,28 @@
 import fetch from "node-fetch";
 import { exit } from "../utils";
 import { URLSearchParams } from "url";
+/**
+ * @interface
+ * Defining the type of the `options` used to initialize the Class.
+ */ 
 interface CMCOptions {
     key: string
 }
+/** 
+ * @interface
+ *  Defining the type of the `options` parameter in the `req` method. 
+ */
 interface reqOptions {
     ext: string
     body: Record<string, unknown>;
 
 }
 
+/**
+ * @class
+ * @classdesc Class used to fetch cryptocurrency information from the CoinMarketCap API.
+ * @param {CMCOptions} options Class Options (CoinMarketCap API Key)
+ */
 export const CMC = class CMC {
     key: string
     url: string
@@ -18,6 +31,12 @@ export const CMC = class CMC {
         this.url = "https://pro-api.coinmarketcap.com/v2/"
     }
 
+/**
+ * Base Fetch Method to CMC API and Authentication
+ * @private
+ * @param {reqOptions} options - reqOptions
+ * @returns Information on Specific Endpoint
+ */
     async req(options: reqOptions){
 				const base_body = {
 			"CMC_PRO_API_KEY":this.key,
@@ -35,6 +54,11 @@ export const CMC = class CMC {
 		}
   }
 
+/**
+ * Fetches information on a specific cryptocurrency market or group of markets via their CoinMarketCap ID E.g 1 for Bitcoin, 2 for Ethereum. To fetch multiple markets seperate each identifier with a comma.
+ * @param {string} id - The id of the cryptocurrency you want to get the quote for.
+ * @returns An object with the following properties:
+ */
     async getQuotesById(id: string) {
       
             const info = this.req({ext: "cryptocurrency/quotes/latest", body: {
@@ -44,6 +68,11 @@ export const CMC = class CMC {
         });
         return info;
     }
+   /**
+    * Fetches information on a specific cryptocurrency market or group of markets via their slug(name) E.g bitcoin, ethereum. To fetch multiple markets seperate each identifier with a comma.
+    * @param {string} slug - The slug of the cryptocurrency you want to get the quotes for.
+    * @returns the info variable.
+    */
     async getQuotesBySlug(slug: string) {
             const info = this.req({ext: "cryptocurrency/quotes/latest", body: {
                 slug: slug,
@@ -52,6 +81,11 @@ export const CMC = class CMC {
         });
         return info;
     }
+/**
+ * Fetches information on a specific cryptocurrency market or group of markets via their symbol E.g BTC, ETH. To fetch multiple markets seperate each identifier with a comma.
+ * @param {string} symbol - The symbol of the cryptocurrency you want to get quotes for.
+ * @returns An object with the following properties:
+ */
     async getQuotesBySymbol(symbol: string) {
             const info = await this.req({ext: "cryptocurrency/quotes/latest", body: {
                 symbol: symbol,
